14f04c31c13338ecfb1e1d9b3be4994735555112
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2020-12-11 10:38:13 -0600

    chagnes bday image

--- js/bot.js
+++ js/bot.js
@@ -148,7 +148,7 @@ client.on('message', msg => {
             let targetchannel = msg.guild.channels.cache.get(realmGeneralChat);
             let taggedUser = msg.mentions.users.first();
             targetchannel.send(`Happy Birthday to you... Happy birthday to you... happy birthday dear ${taggedUser}........ happy birthday to you!`, {
-                files: ["images/birthday_gif.webp"]
+                files: ["images/bday.gif"]
             }); 
         }
         catch {
